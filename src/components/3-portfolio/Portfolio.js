5c84f743da6c711a4f8c679f47039eb8c87e330c
Kifli <zoalkeflabunday@gmail.com>
2025-03-13 01:46:49 +0200

    adding dark and light mode

--- src/components/3-portfolio/Portfolio.js
+++ src/components/3-portfolio/Portfolio.js
@@ -1,6 +1,10 @@
 import Titel from "../home/titel";
+import project1 from "../../assets/projects/app-1.jpg";
 import "./portfolio.css";
+import { useState } from "react";
+
 const Portfolio = () => {
+  const [currentActive , setcurrentActive] = useState("all")
   return (
     <div className="portfolio">
       <Titel name="recntale worck" />
@@ -8,24 +12,36 @@ const Portfolio = () => {
       <div className="box">
         <div>
           <ul>
-            <li>All Projects</li>
-            <li>HTML & CSS</li>
-            <li>JavaScript</li>
-            <li>Bootsrap</li>
-            <li>React</li>
+            <li onClick={()=>{
+              setcurrentActive("all")
+            }} className={currentActive === "all" ? "active" : null}>All Projects</li>
+            <li onClick={()=>{
+              setcurrentActive("HTML & CSS")
+            }} className={currentActive === "HTML & CSS" ? "active" : null}>HTML & CSS</li>
+            <li onClick={()=>{
+              setcurrentActive("JavaScript")
+            }} className={currentActive === "JavaScript" ? "active" : null}>JavaScript</li>
+            <li onClick={()=>{
+              setcurrentActive("Bootsrap")
+            }} className={currentActive === "Bootsrap" ? "active" : null}>Bootsrap</li>
+            <li onClick={()=>{
+              setcurrentActive("React")
+            }} className={currentActive === "React" ? "active" : null}>React</li>
           </ul>
         </div>
         <div className="projects">
           <div className="project">
-            <div className="pr_imeg"></div>
-            <h4>titel</h4>
+            <div className="pr_imeg">
+              <img src={project1} alt={project1}/>
+            </div>
+            <h4>product also</h4>
             <p>
               user-friendly but also visually appealing. I add a personal touch
               to your product, ensuring it’s eye-catching and
             </p>
             <div className="pr_links">
-              <div className="demo">demo</div>
-              <div className="github">github</div>
+              <div className="demo"><a href="/">demo</a></div>
+              <div className="github"><a href="/">github</a></div>
             </div>
           </div>
         </div>
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,80 +1,112 @@
 import { Routes, Route, Link } from "react-router-dom";
 import { FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { PiReadCvLogoLight } from "react-icons/pi";
 import { MdEmail } from "react-icons/md";
+import { IoHome } from "react-icons/io5";
 import { GoMoon } from "react-icons/go";
 import { LuSunMedium } from "react-icons/lu";
+import { MdContactPhone } from "react-icons/md";
 import About from "../1-about/About";
 import Portfolio from "../3-portfolio/Portfolio";
 import Contact from "../4-contact/Contact";
 import MyImg from "../../assets/me.jpg";
 import "./home.css";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 
 function Home() {
-  const [AddClass , SetAddClass] = useState(true);
-   
+  const [AddClass, SetAddClass] = useState(true);
+  const [Mymode, SetMymode] = useState(localStorage.getItem("currentmode") ?? "dark");
+useEffect(()=>{
+  if(Mymode === "light"){
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+  }else{
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+  }
+})
   return (
     <div className="home">
       <div className="container">
-       {AddClass &&(
-         <div className="left">
-          <div className="avatar">
-            <div className="my_img">
-              <img src={MyImg} alt={MyImg} />
+        {AddClass && (
+          <div className="left">
+            <div className="avatar">
+              <div className="my_img">
+                <img src={MyImg} alt={MyImg} />
+              </div>
+              <div className="my_name">
+                <h3>zulkifli ahmed</h3>
+                <h5>front end developer</h5>
+              </div>
             </div>
-            <div className="my_name">
-              <h3>zulkifli ahmed</h3>
-              <h5>front end developer</h5>
+            <div className="my_gap"></div>
+            <div className="my-links">
+              <a href="/">
+                <FaGithub />
+              </a>
+
+              <a href="/">
+                <FaLinkedin />
+              </a>
+
+              <a href="/">
+                <FaFacebook />
+              </a>
+              <a href="/">
+                <MdEmail />
+              </a>
             </div>
           </div>
-          <div className="my_gap"></div>
-          <div className="my-links">
-            <a href="/">
-              <FaGithub />
-            </a>
-
-            <a href="/">
-              <FaLinkedin />
-            </a>
-
-            <a href="/">
-              <FaFacebook />
-            </a>
-            <a href="/">
-              <MdEmail />
-            </a>
-          </div>
-        </div>
-       )}
+        )}
 
         <div className="right">
           <nav>
             <ul>
               <li>
-                <Link  onClick={()=>{
-                  SetAddClass(true)
-                }}  to={"/about"}>about</Link>
+                <Link
+                  onClick={() => {
+                    SetAddClass(true);
+                  }}
+                  to={"/about"}
+                >
+                  <IoHome  className="icon_nav" /> <span>about</span>
+                </Link>
               </li>
               <li>
-                <Link onClick={()=>{
-                  SetAddClass(false)
-                }} to={"/portfolio"}>portfolio</Link>
+                <Link
+                  onClick={() => {
+                    SetAddClass(false);
+                  }}
+                  to={"/portfolio"}
+                >
+                  {" "}
+                  <FaLaptopCode className="icon_nav" /> <span>portfolio</span>
+                </Link>
               </li>
               <li>
-                <Link  onClick={()=>{
-                  SetAddClass(true)
-                }}  to={"/contact"}>contact</Link>
+                <Link
+                  onClick={() => {
+                    SetAddClass(true);
+                  }}
+                  to={"/contact"}
+                >
+                  <MdContactPhone className="icon_nav" /> <span>contact</span>{" "}
+                </Link>
               </li>
               <li>
-                <a href="/" target="_blank">
-                  cv
-                </a>
+                <Link href="/" target="_blank">
+                  <PiReadCvLogoLight className="icon_nav" /> <span>my cv</span>
+                </Link>
               </li>
             </ul>
-            <div className="my-mode">
-               <LuSunMedium />
-               <GoMoon />
+            <div className="my-mode" onClick={()=>{
+                 localStorage.setItem("currentmode", Mymode === "dark" ?"light" :"dark");
+                 SetMymode(localStorage.getItem("currentmode"))
+              }}>
+              {Mymode === "dark" ?(<GoMoon className="moon"/>) :(<LuSunMedium className="sun"/>)}
+              
+              
             </div>
           </nav>
           <div>
